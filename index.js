8388155ac7e162da10dae377e0214cf1dfa20c9c
David Divilly <d.divilly@gmail.com>
2017-12-10 21:04:41 +0000

    Added bodyparser

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 //set up express app
 const app = express();
 
+// only receiving json format
+// above the routes so routes is then able to use it
+app.use(bodyParser.json());
+
 // using the routes from the api dir
 // all routes are going to start /api
+// initialise the routes
 app.use('/api',require('./routes/api'));
 
 //listening for requests
