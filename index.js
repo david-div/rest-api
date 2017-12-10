418abf0632c51429a59e5da9e46e845585cbaf94
David Divilly <d.divilly@gmail.com>
2017-12-10 19:54:29 +0000

    Added CRUD routes

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require('express');
 
 //set up express app
 const app = express();
-
-app.get('/api',function(req, res){
-  console.log('GET request');
-  res.send({ name: 'Yoshi'});
-});
-
 //listening for requests
 // either the set up variable or port 4000
 app.listen(process.env.port || 4000, function(){
---
+++ routes/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+// get a list of ninjas from the db
+router.get('/ninjas', function(req, res){
+  res.send({type: 'GET'});
+});
+
+// add a new ninja to the db
+router.post('/ninjas', function(req, res){
+  res.send({type: 'POST'});
+});
+
+// update a ninja in the db
+// :id is going to be a varialbe name
+router.put('/ninjas/:id', function(req, res){
+  res.send({type: 'PUT'});
+});
+
+// delete a ninja from the db
+router.delete('/ninjas/:id', function(req, res){
+  res.send({type: 'DELETE'});
+});
