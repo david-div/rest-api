40c1ee6aced53be92b7fd7c49fa03852f428417c
David Divilly <d.divilly@gmail.com>
2017-12-11 18:50:03 +0000

    Added geolocation to schema

--- models/ninja.js
+++ models/ninja.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+"geometry": {
+  "type": "Point",
+  "coordinates": [125.6, 10.1]
+}
+
+// create geolocation schema
+
+const GeoSchema = new Schema({
+  type: {
+    type: String,
+    default: "Point"
+  };
+  coordinates: {
+    type: [Number],
+    // type of map we want to use, 'sphere' takes the 3d of the world into consideration
+    index: "2dsphere"
+  }
+})
+
 // create ninja Schema & model
 const NinjaSchema = new Schema({
   name: {
@@ -14,8 +33,9 @@ const NinjaSchema = new Schema({
   available: {
     type: Boolean,
     default: false
-  }
-  // add in geo location
+  },
+  // from the GeoSchema schema
+  geometry: GeoSchema
 });
 
 // creating a model
